fd7e6a5c83b067839d036c9a8d760575f1cbf591
Jhafet19 <absarga96@gmail.com>
2023-12-30 19:20:59 -0600

    V1
    V!

---
+++ src/main/java/com/algar/absa/PruebaAlgar/PruebaAlgarApplication.java
@@ -0,0 +1,13 @@
+package com.algar.absa.PruebaAlgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAlgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAlgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algar/absa/PruebaAlgar/PruebaAlgarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algar.absa.PruebaAlgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAlgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
